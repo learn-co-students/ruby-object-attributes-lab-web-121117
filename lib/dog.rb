41697194c12581228cbb8b761e2a426f2f320fa5
olegchursin <olegchursin@gmail.com>
2017-12-13 09:43:41 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed
+    @breed
+  end
+
+  # attr_accessor :breed #both getter and setter
+  # # attr_reader :breed #getter method
+  # # attr_writer :breed #setter method
+
+end
